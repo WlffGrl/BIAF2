9ac78104eb89fcf020301807c75debba424e174e
Husein Haikal Muhammad <haikalhusein57@gmail.com>
2022-07-31 22:45:23 +0700

    Footer doeang ampe 5 jam

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,35 +1,17 @@
 <!-- Navbar -->
-<div class="logo bg-primary p-5">
-    <h1>
-        <img src="img/logo2020.png" class="rounded mx-auto d-block" alt="BIAF">
-    </h1>
-</div>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto  text-uppercase">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">BIAF</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pengumuman">Pengumuman</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/jadwal">Jadwal</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="/forum">Forum</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="">index</a>
-                </li>
-            </ul>
-        </div>
+<nav class="navbar navbar-dark bg-primary navbar-expand-md">
+    <img src="img/logo2020.png" alt="">
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navbar">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a href="/" class="nav-link">BIAF</a></li>
+            <li class="nav-item"><a href="/pengumuman" class="nav-link">PENGUMUMAN</a></li>
+            <li class="nav-item"><a href="/jadwal" class="nav-link">JADWAL</a></li>
+            <li class="nav-item"><a href="/forum" class="nav-link">FORUM</a></li>
+            <li class="nav-item"><a href="/index" class="nav-link">INDEX PENGUKURAN</a></li>
+        </ul>
     </div>
 </nav>
 <!-- End Navbar -->
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -25,6 +25,9 @@
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
     <!-- Navbar -->
     @include('inc.nav')
     <!-- End Navbar -->
@@ -48,11 +51,137 @@
     <!-- End Content -->
 
     <!-- Footer -->
-    <footer class="bg-primary md-4 p-5 text-light text-center">
-        <img src="img/logo2020.png" class="rounded mx-auto d-block" alt="BIAF">
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis quis turpis sollicitudin
-            malesuada. Donec vel ex eu tortor.</p>
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #3e89f2">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-twitter"></i></a>
+
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-google"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-linkedin-in"></i></a>
+
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-github"></i></a>
+            </section>
+
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Supported By</h5>
+                        <div class="row justify-content-evenly">
+                            <div class="col-lg-5 col-md-6 mb-4 mb-md-0 p-3">
+                                <img src="img/pemkot.png" alt="" width="80" height="">
+                            </div>
+                            <div class="col-lg-5 col-md-6 mb-4 mb-md-0 p-3">
+                                <img src="img/pmptsp.png" alt="" width="190" height="">
+                            </div>
+                            <div class="col-lg-5 col-md-6 mb-4 mb-md-0 p-3">
+                                <img src="img/LogoBKPM.png" alt="" width="100" height="">
+                            </div>
+                            <div class="col-lg-5 col-md-6 mb-4 mb-md-0 p-3">
+                                <img src="img/logo-dpmptsp.png" alt="" width="190" height="">
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3 bg-primary text-light">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
     </footer>
+    <!-- Footer -->
     <!-- End Footer -->
 </body>
 
